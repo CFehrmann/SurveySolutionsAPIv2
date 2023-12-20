7ca97b3668e198e4194abff72cd0c7d0e0c5adcd
Michael Wild <mwild@worldbank.org>
2023-12-20 17:38:40 -0300

    added global options

---
+++ R/suso_options.R
@@ -0,0 +1,17 @@
+#' Configuration options used for the SurveySolutionsAPIv2 package
+#'
+#' @description
+#' Below are the options and environment variables, that are used by the \pkg{SurveySolutionsAPIv2} package.
+#'
+#' @section Available options:
+#' Bellow is a list of options, with their default values.
+#'
+#' \itemize{
+#'   \item \option{max.par.requests} specifies the maximum number of parallel requests, default is 100
+#'   \item \option{max.par.connections} specifies the maximum number of parallel connections,
+#'   and must always be equal or greater than max.par.requests, default is 100.
+#' }
+#'
+#' @name suso.options
+#'
+NULL
