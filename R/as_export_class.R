86ee5100525335a94ebb9a16c5f5aa9eaf2fe6f0
Michael Wild <mwild@worldbank.org>
2024-01-10 10:25:07 -0300

    updated documentaton exportClass paradata

--- R/as_export_class.R
+++ R/as_export_class.R
@@ -178,23 +178,30 @@ is.exportClass<-function(x) {
 #' This function generates a summary table of numeric variables in an exportClass
 #' object.
 #'
-#' @param x A exportClass object.
+#' @param x exportClass object.
 #' @param ... further arguments.
+#'
+#' @return A DataTable (DT) object (HTML table) displaying the summary statistics, if \code{useDT = TRUE}, and a plain data.table otherwise.
+#'
+#' @export
+summaryTable <- function(x , ... ){ #,includeFactors = FALSE, useDT = TRUE,  DTstyle = TRUE) {
+  UseMethod("summaryTable")
+}
+
+
+#' @name summaryTable
 #' @param includeFactors if \code{TRUE}, factor variables will be converted to numeric and included in the table.
 #' @param useDT Logical, if TRUE (default) the function returns a DataTable (DT) object (HTML table), otherwise
 #' a data.table object is returned.
 #' @param DTstyle if \code{TRUE} and \code{useDT = TRUE}, then a custom style will be applied, otherwise a plain DT table will be returned.
 #'
-#' @return A DataTable (DT) object (HTML table) displaying the summary statistics, if \code{useDT = TRUE}, and a plain data.table otherwise.
 #'
 #' @details
 #' \itemize{
 #' \item For the main data: The table includes statistics such as mean, standard deviation, maximum, minimum, and total
 #' count as well as count of NA values.
-#' \item For paradata: Different summary tables for response times, duration times etc. see examples.
 #' }
 #'
-#' @export
 #'
 #' @examples
 #' \dontrun{
@@ -212,35 +219,12 @@ is.exportClass<-function(x) {
 #' # Display the summary table
 #' summary_data_table
 #'
-#' ### PARADATA
-#' para<-suso_export_paradata(questID = questlist$QuestionnaireId[1],
-#'                           version = questlist$Version[1], reloadTimeDiff = 24,
-#'                           workStatus = "All", asList = F, onlyActiveEvents = T)
-#'
-#' # Create a summary table of all event counts by action (includes always passive and active one)
-#' actions <- summaryTable(para, "action")
-#'
-#' # Create a summary table of all event counts by user
-#' users <- summaryTable(para, "user")
-#'
-#' # Create a summary table of the average response time by variable
-#' resptime_var <- summaryTable(para, "VariableName")
-#'
-#' # Create a summary table of the average response time by question type
-#' resptime_type <- summaryTable(para, "type")
-#'
-#' # Create a summary table of the average response time by user
-#' resptime_user <- summaryTable(para, "responsible")
 #'
 #'
 #' }
 #'
-summaryTable <- function(x , ... ,includeFactors = FALSE, useDT = TRUE,  DTstyle = TRUE) {
-  UseMethod("summaryTable")
-}
-
-
 #' @export
+
 summaryTable.exportClass <- function(x, ... ,includeFactors = FALSE, useDT = TRUE, DTstyle = TRUE) {
 
   # main data export
@@ -370,6 +354,239 @@ summaryTable.exportClass <- function(x, ... ,includeFactors = FALSE, useDT = TRU
       # 1. response time by ...
         cond<-rlang::parse_expr(sprintf('%s!= "<NA>"', arg))
         summary_data<-x[eval(cond),.(`Av. Response Time (in seconds)` = mean(resp_time, na.rm = T)), by= arg]
+
+    }
+
+    ## round values
+    # Get the numeric columns
+    numeric_cols <- names(summary_data)[sapply(summary_data, is.numeric)]
+    ## remove Itime by name for now
+    numeric_cols<-numeric_cols[numeric_cols!="time"]
+    #dig <- if(!is.null(list(...)$digits)) list(...)$digits else 3
+    summary_data<-summary_data[,(numeric_cols) := lapply(.SD, round, 3), .SDcols = numeric_cols]
+
+    if (!useDT) {
+      # Return plain data.table
+      return(summary_data)
+    } else {
+      # Return DT
+      # cli abort if DT is not found
+      if (!(rlang::is_installed("DT"))) {
+        cli::cli_abort(c("x" ="DT package is required to use this function and not found on your system."))
+      }
+      # GEN DT
+      # define options
+      if(DTstyle) {
+        smTabDir <- list(
+          dom = "t",
+          pagelength = nrow(summary_data),
+          paging = FALSE,
+          # dom = 'Blft',
+          fixedHeader = TRUE,
+          # Solution for rowwise from: https://stackoverflow.com/questions/69812528/r-data-table-alternating-row-color-with-groupings
+          initComplete = DT::JS(
+            "function(settings, json) {",
+            "$(this.api().table().header()).css({'background-color': '#002244', 'color': '#FFFFFF'});",
+            "$('table.dataTable.display tbody tr.odd').css('background-color', '#FFFFFF');",
+            "$('table.dataTable.display tbody tr.even').css('background-color', '#009FDA80');",
+            "}"),
+          columnDefs = list(list(className = 'dt-left', targets = c(0,1)))
+        )
+      } else {
+        smTabDir <- list(
+          pagelength = nrow(summary_data),
+          paging = FALSE
+        )
+      }
+
+      # render
+      DT<-DT::datatable(summary_data,
+                        smTabDir, rownames = F
+                        #style = "bootstrap4"
+      ) |>
+        DT::formatStyle(1:length(summary_data),
+                        color = '#002244',
+                        #backgroundColor = 'orange',
+                        fontWeight = 'bold')
+
+      # DT<-DT::datatable(summary_data)
+      return(DT)
+    }
+
+  }
+
+}
+
+#' @name summaryTable
+#' @param useDT Logical, if TRUE (default) the function returns a DataTable (DT) object (HTML table), otherwise
+#' a data.table object is returned.
+#' @param DTstyle if \code{TRUE} and \code{useDT = TRUE}, then a custom style will be applied, otherwise a plain DT table will be returned.
+#'
+#'
+#' @details
+#' \itemize{
+#' \item For paradata: Different summary tables for response times, duration times etc. see examples.
+#' }
+#'
+#'
+#' @examples
+#' \dontrun{
+#' questlist<-suso_getQuestDetails()
+#'
+#' ### PARADATA
+#' para<-suso_export_paradata(questID = questlist$QuestionnaireId[1],
+#'                           version = questlist$Version[1], reloadTimeDiff = 24,
+#'                           workStatus = "All", asList = F, onlyActiveEvents = T)
+#'
+#' # Create a summary table of all event counts by action (includes always passive and active one)
+#' actions <- summaryTable(para, "action")
+#'
+#' # Create a summary table of all event counts by user
+#' users <- summaryTable(para, "user")
+#'
+#' # Create a summary table of the average response time by variable
+#' resptime_var <- summaryTable(para, "VariableName")
+#'
+#' # Create a summary table of the average response time by question type
+#' resptime_type <- summaryTable(para, "type")
+#'
+#' # Create a summary table of the average response time by user
+#' resptime_user <- summaryTable(para, "responsible")
+#'
+#'
+#' }
+#'
+#' @export
+
+summaryTable.paradata <- function(x, ... ,useDT = TRUE, DTstyle = TRUE) {
+
+  # main data export
+  if(!getinfo(x, "isPara")){
+
+    # check if x is of class exportClass
+    if (!is.exportClass(x)) {
+      cli::cli_abort(c("x" = "x must be of class exportClass."))
+    }
+
+    # Get the numeric columns
+    numeric_cols <- names(x)[sapply(x, is.numeric)]
+    # transform factors and add
+
+    # !!! Currently gives error with additonal NA values --> check
+    # if(includeFactors) {
+    if(FALSE) {
+      factor_cols <- names(x)[sapply(x, is.factor)]
+      x<-x[,lapply(.SD, as.numeric), .SDcols = factor_cols]
+      # update numeric cols
+      numeric_cols <- c(numeric_cols, factor_cols)
+
+    }
+    # exclude survey solutions system columns
+    numeric_cols <- .excludeSysVars(numeric_cols)
+
+    # If no nummeric columns are found return NULL and cli warning
+    if (length(numeric_cols) == 0) {
+      cli::cli_alert_warning("No numeric columns found in the data.")
+      return(NULL)
+    }
+
+    # Get the additional NA values
+    additional_na_values <- attr(x, "na_numeric")
+
+
+    # Initialize an empty data.table for the summary
+    summary_data <- data.table(
+      Variable = character(0),
+      Label = character(0),
+      Mean = numeric(0),
+      SD = numeric(0),
+      Max = numeric(0),
+      Min = numeric(0),
+      Count = numeric(0),
+      NA_Count = numeric(0)
+    )
+    # set decimal digits to 3 with withr or use ...$digits if provided
+    dig <- if(!is.null(list(...)$digits)) list(...)$digits else 3
+    # Calculate summary statistics for each numeric column
+    for (col in numeric_cols) {
+      # replace additional NA values with NA
+      x[[col]][x[[col]] == additional_na_values] <- NA
+      summary_data <- data.table::rbindlist(
+        list(summary_data,
+             data.table(
+               Variable = col,
+               Label = attr(x, tolower(col)),
+               Mean = round(mean(x[[col]], na.rm = TRUE), digits = dig),
+               SD = round(sd(x[[col]], na.rm = TRUE), digits = dig),
+               Max = round(max(x[[col]], na.rm = TRUE), digits = dig),
+               Min = round(min(x[[col]], na.rm = TRUE), digits = dig),
+               count = length(x[[col]]),
+               NA_Count = sum(is.na(x[[col]]))
+             )), fill = TRUE
+      )
+    }
+
+    # If useDT is FALSE return the data.table
+    if (!useDT) {
+      # Return plain data.table
+      return(summary_data)
+    } else {
+      # Return DT
+      # cli abort if DT is not found
+      if (!(rlang::is_installed("DT"))) {
+        cli::cli_abort(c("x" ="DT package is required to use this function and not found on your system."))
+      }
+      # GEN DT
+      # define options
+      if(DTstyle) {
+        smTabDir <- list(
+          dom = "t",
+          pagelength = nrow(summary_data),
+          paging = FALSE,
+          # dom = 'Blft',
+          fixedHeader = TRUE,
+          # Solution for rowwise from: https://stackoverflow.com/questions/69812528/r-data-table-alternating-row-color-with-groupings
+          initComplete = DT::JS(
+            "function(settings, json) {",
+            "$(this.api().table().header()).css({'background-color': '#002244', 'color': '#FFFFFF'});",
+            "$('table.dataTable.display tbody tr.odd').css('background-color', '#FFFFFF');",
+            "$('table.dataTable.display tbody tr.even').css('background-color', '#009FDA80');",
+            "}"),
+          columnDefs = list(list(className = 'dt-center', targets = c(0,1)))
+        )
+      } else {
+        smTabDir <- list(
+          pagelength = nrow(summary_data),
+          paging = FALSE
+        )
+      }
+
+      # render
+      DT<-DT::datatable(summary_data,
+                        smTabDir, rownames = F
+                        #style = "bootstrap4"
+      ) |>
+        DT::formatStyle(1:length(summary_data),
+                        color = '#002244',
+                        #backgroundColor = 'orange',
+                        fontWeight = 'bold')
+
+      # DT<-DT::datatable(summary_data)
+      return(DT)
+    }
+  } else if(getinfo(x, "isPara")) {
+    # PARADATA
+    args<-rlang::list2(...)
+    arg<-as.character(args)
+    arg<-rlang::set_names(arg, names(args))
+    if(arg %in% c("action", "user", "role")){
+      # already processesd
+      summary_data<-getinfo(x, arg = arg)
+    } else {
+      ## calculate new...
+      # 1. response time by ...
+      cond<-rlang::parse_expr(sprintf('%s!= "<NA>"', arg))
+      summary_data<-x[eval(cond),.(`Av. Response Time (in seconds)` = mean(resp_time, na.rm = T)), by= arg]
 
     }
 
